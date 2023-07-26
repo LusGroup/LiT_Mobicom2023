a7e1245e94d2f142bd563d0a7b0c30ed8d202195
Lu's Group <lusgroup2015@gmail.com>
2023-07-26 21:54:07 +0800

    Update Step1_collect_data.m

--- Step1_collect_data.m
+++ Step1_collect_data.m
@@ -6,7 +6,7 @@
 %collected data is displayed in real time.
 close all;
 close();
-% fclose(instrfind);
+fclose(instrfind);
 s = serial('COM5','BaudRate',9600);  
 %定义串口对象 Define serial port objects (Note that, this port (first parameter) depends on the Arduino UNO port recognized by the computer)
 
@@ -14,29 +14,34 @@ s = serial('COM5','BaudRate',9600);
 fopen(s);  %打开串口对象s Open serial port object s
 
 t=0;
-x=0;
-y=0;
-% z=0;
-% j=0;
+y1=0;
+y2=0;
+figure('Position', [100, 100, 1280, 600]);
+
 interval = 2400;%sampling number (Note that, sampling rate is 10 Hz)
 while(t<interval)
     b=str2num(fgetl(s));
     if length(b)==2
     a4=b(1);
     a5=b(2);
-        x=[x,a5];
-        y=[y,a4];
+        y1=[y1,a5];
+        y2=[y2,a4];
     else
-        break;
+        fclose(instrfind);
+        fclose(s);
+        
+        fopen(s); %b=str2num(fgetl(s));
+%         break;
     end
-    plot(x,'-r', 'LineWidth',2);
+    if ~isempty(y1) && ~isempty(y2)
+    plot(y1,'-b', 'LineWidth',2);
     hold on;
-    plot(y,'-.b', 'LineWidth',2);
-
+    plot(y2,'-.r', 'LineWidth',2);
+    end
     t=t+1;
     drawnow;
 end
-%The data of the two sensors are stored in separate mat files.
-save("E:\toothbrush\原始数据\原始数据3\sensor1.mat", "x");% Path to save sensor 1's data
-save("E:\toothbrush\原始数据\原始数据3\sensor2.mat", "y");% Path to save sensor 2's data
+%The data of the two sensors are stored in mat files.
+save("data\brushingsurface1.mat", "y1","y2");% Path to save sensor data
+
 fclose(s);
